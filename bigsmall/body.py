7e72a1d022b809126b61e9390608e4904aa9bbc8
Roman <44116985+romanfinkin@users.noreply.github.com>
2019-02-07 00:35:37 +0300

    +

--- bigsmall/body.py
+++ bigsmall/body.py
@@ -15,16 +15,22 @@ def lost():
     print("big number",z)
     print("small number ", x)
 
-
-
-
-
- #   s = s1[0]
- #   x = s1[1]
- #   print("s=", s)
- #   if x>s :
- #       s=x
- #   print("s=", s)
-
-
 lost()
+
+###################################################
+#x = int(input('Enter number '))
+#
+#xmin = x % 10
+#xmax = x % 10
+#x //= 10
+#
+#while x > 0:
+#    z = x % 10
+#    if z < xmin:
+#        xmin = z
+#    if z > xmax:
+#        xmax = z
+#    x //= 10
+#
+#print('max number -> %d' % xmin)
+#print('min number -> %d' % xmax)
