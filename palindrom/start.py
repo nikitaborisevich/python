36e1b9b4578b780bd23022596a0d69a4dfda041d
Roman <44116985+romanfinkin@users.noreply.github.com>
2019-02-07 00:55:26 +0300

    +

--- palindrom/start.py
+++ palindrom/start.py
@@ -11,3 +11,22 @@ def start():
 
 
 start()
+
+
+
+#s = input("enter ")
+#
+#
+#def start(s1):
+#    x = s1
+#    x = x[::-1]
+#    if x == s1:
+#        return True
+#    else:
+#        return False
+#
+#
+#if start(s):
+#    print('+')
+#else:
+#    print('-')
